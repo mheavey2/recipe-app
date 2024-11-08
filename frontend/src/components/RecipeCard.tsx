56a5a67ace244bec93ecf1cb290738c740ac2676
mheavey2 <mheavey2@gmail.com>
2024-11-08 11:38:37 +0000

    add recipe card and recipe modal components

---
+++ frontend/src/components/RecipeCard.tsx
@@ -0,0 +1,21 @@
+import { Recipe } from "../types";
+
+interface Props {
+  recipe: Recipe;
+  // define that the onClick prop thats passed to RecipeCard has to be a function that doesn't return anything
+  onClick: () => void;
+}
+
+const RecipeCard = ({ recipe, onClick }: Props) => {
+  return (
+    <div className="recipe-card" onClick={onClick}>
+      <img src={recipe.image} alt="recipe image" />
+      <div className="recipe-card-title">
+        <h3>{recipe.title}</h3>
+        {/* <h6>{recipe.id}</h6> */}
+      </div>
+    </div>
+  );
+};
+
+export default RecipeCard;
