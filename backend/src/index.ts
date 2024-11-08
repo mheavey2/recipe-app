f85ceea06c9d51563508c0806243b84bebd8c361
mheavey2 <mheavey2@gmail.com>
2024-11-08 11:17:15 +0000

    add route to handle requests to recipe search endpoint. add route to handle requests to recipe summary endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,12 +8,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//route to handle requests to recipe search endpoint
 app.get("/api/recipe/search", async (req, res) => {
   const searchTerm = req.query.searchTerm as string;
   const page = parseInt(req.query.page as string);
   const results = await RecipeAPI.searchRecipes(searchTerm, page);
 
   return res.json(results);
+});
+
+// route to handle requests to recipe summary endpoint
+// :recipeId means its a path param
+app.get("/api/recipe/:recipeId/summary", async (req, res) => {
+  const recipeId = req.params.recipeId;
+  const result = await RecipeAPI.getRecipeSummary(recipeId);
+  res.json(result);
 });
 
 app.listen(3000, () => {
